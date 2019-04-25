e62c92910f4ce73080bbc1e916604aa7de73fae7
AshwinRam2000 <ramashwin36@gamil.com>
2019-04-25 09:11:17 +0530

    update

--- GUVI/createDetail.php
+++ GUVI/createDetail.php
@@ -15,13 +15,41 @@ include_once("DBConnection.php");
 
 
 
- $inuser = $_POST["user"]; 
-$stmt= $db->prepare("INSERT INTO PROFILE1 (TITLE,FIRSTNAME,LASTNAME,POSITION,COMPANY,BUSINESSAREA,EMPLOYEE,STREET,ADDITIONALINFO,ZIP,PLACE,CODE,PHONE,EMAIL) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?) "); //Fetching all the records with input credentials
+ 
+//$stmt= $db->prepare("INSERT INTO PROFILE1 (TITLE,FIRSTNAME,LASTNAME,POSITION,COMPANY,BUSINESSAREA,EMPLOYEE,STREET,ADDITIONALINFO,ZIP,PLACE,CODE,PHONE,EMAIL) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?) "); //Fetching all the records with input credentials
   //bind_param() - Binds variables to a prepared statement as parameters. "s" indicates the type of the parameter.
 
 
 
 //$stmt->bind_param("ssssssssssssss",$inTITLE,$inFIRSTNAME,$inLASTNAME,$inPOSITION,$inCOMPANY,$inBUSINESSAREA,$inEMPLOYEE,$inSTREET,$inADDITIONALINFO,$inZIP,$inPLACE,$inCODE,$inPHONE,$inEMAIL);
+
+
+
+//$stmt= $db->prepare("UPDATE PROFILE1 SET TITLE,FIRSTNAME,LASTNAME,POSITION,COMPANY,BUSINESSAREA,EMPLOYEE,STREET,ADDITIONALINFO,ZIP,PLACE,CODE,PHONE,EMAIL ");
+
+
+$stmt= $db->prepare("UPDATE PROFILE1 SET TITLE=?,FIRSTNAME=?,LASTNAME=?,POSITION=?,COMPANY=?,BUSINESSAREA=? ,EMPLOYEE=?,STREET=?,ADDITIONALINFO=?,ZIP=?,PLACE=?,CODE=?,PHONE=?,EMAIL=? WHERE USERNAME= ?");
+$stmt->bind_param("sssssssssssssss",$inTITLE,$inFIRSTNAME,$inLASTNAME,$inPOSITION,$inCOMPANY,$inBUSINESSAREA,$inEMPLOYEE,$inSTREET,$inADDITIONALINFO,$inZIP,$inPLACE,$inCODE,$inPHONE,$inEMAIL,$inUSERNAME);
+
+
+$inTITLE=$_POST["title"];
+$inFIRSTNAME=$_POST["first_name"];
+$inLASTNAME=$_POST["last_name"];
+$inPOSITION=$_POST["position"];
+$inCOMPANY=$_POST["company"];
+$inBUSINESSAREA=$_POST["business"];
+$inEMPLOYEE=$_POST["employees"];
+$inSTREET=$_POST["street"];
+$inADDITIONALINFO=$_POST["additional"];
+$inZIP=$_POST["zip"];
+$inPLACE=$_POST["place"];
+$inCODE=$_POST["code"];
+$inPHONE=$_POST["phone"];
+$inEMAIL=$_POST["your_email"];
+$inUSERNAME = $_POST["user"]; 
+/* TITLE='$inTITLE'
+FIRSTNAME='$inFIRSTNAME'
+ USERNAME= '$inuser' */
 
 $stmt->execute();
 
